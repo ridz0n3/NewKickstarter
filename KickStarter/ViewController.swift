2bdc5477da0391a56b6d398aa0a13f4e7cfc9f6c
ridzuan <ridz0n391@gmail.com>
2017-01-04 18:05:03 +0800

    Update

---
+++ KickStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KickStarter
+//
+//  Created by MacBook 1 on 04/01/2017.
+//  Copyright © 2017 MacBook 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
